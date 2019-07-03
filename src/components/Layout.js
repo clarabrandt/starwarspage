5594298fc220605ab343c67aa8a52339483ceca2
clarabrandt <mariaclarabrandt@gmail.com>
2019-07-03 18:03:51 +0200

    clean up and create layout component

---
+++ src/components/Layout.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import "./Layout.css";
+
+
+class Layout extends Component {
+  render() {
+    return (
+      <div className='layout'>
+
+      </div>
+    )
+  }
+}
+
+
+export default Layout
