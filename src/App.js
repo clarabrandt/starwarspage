3a3eeb491941fdc1e03b2c3644f7be25a54de2e4
clarabrandt <mariaclarabrandt@gmail.com>
2019-07-03 18:07:34 +0200

    import Layout to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Layout from "./components/Layout.js";
 
 import './App.css';
 
 function App() {
   return (
     <div className="app">
-
+      <Layout />
     </div>
   );
 }
