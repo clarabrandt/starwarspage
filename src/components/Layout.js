657b29dfd94a21f0ed11b02abdc9de96342a7af2
clarabrandt <mariaclarabrandt@gmail.com>
2019-07-03 18:10:28 +0200

    creating component Banner and importing in Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 import "./Layout.css";
+import Banner from './Banner'
 
 
 class Layout extends Component {
   render() {
     return (
       <div className='layout'>
-
+        <Banner />
       </div>
     )
   }
