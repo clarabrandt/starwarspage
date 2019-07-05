68f38402fbc1695f8c354bf7f2ab3964f717a3a4
clarabrandt <mariaclarabrandt@gmail.com>
2019-07-05 21:26:45 +0200

    layot for characters list

--- src/components/Content.js
+++ src/components/Content.js
@@ -8,7 +8,7 @@ export default class Content extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: 'force',
+      value: '',
       order: 'ASC',
       people: [],
     };
@@ -60,13 +60,18 @@ export default class Content extends Component {
   renderList() {
     const { people } = this.state;
     return (
-      <div>
+      <div className='content-list--items'>
+        <div className='content-list--items-item'>
+          <div className='content-list--items-name bold'>Name</div>
+          <div className='content-list--items-height bold clickable' onClick={this.toggleButton}>Height &#x25B2;&#x25BC;</div>
+        </div>
         {
           this.state.people &&
           people.map(person => {
             return (
-              <div className=''>
-                {`${person.name} - ${person.height}`}
+              <div className='content-list--items-item'>
+                <div className='content-list--items-name'>{person.name}</div>
+                <div className='content-list--items-height'>{person.height}</div>
               </div>
             )
           })
@@ -78,20 +83,18 @@ export default class Content extends Component {
   render() {
     return (
       <div className='content'>
-        <div className='content-headline'>Want to know what are the characters on each Star Wars movie? </div>
-        <div className='content-headline'>Search using a word from the title your favorite episode! </div>
-        {
-          <>
+        <div className='content-headline'>
+          <h3>Want to know what are the characters on each Star Wars movie?</h3>
+          <h4>Search using a word from the title your favorite episode!</h4>
+        </div>
+        <div className='content-body'>
+          <div className='content-form'>
             <label className='content-input'>
-              <div className='content-input--title'>Search word</div>
-              <input type="text" value={this.state.value} onChange={this.handleChange} />
+              <input className='content-input--field' placeholder="search term" type="text" value={this.state.value} onChange={this.handleChange} />
             </label>
-            <input type="submit" value="search" onClick={this.searchData} />
-            <button onClick={this.toggleButton}>
-              {this.state.order}
-            </button>
-          </>
-        }
+            <input className='content-input--button' type="submit" value="search" onClick={this.searchData} />
+          </div>
+        </div>
         <div className='content-list'>
           {this.renderList()}
         </div>
