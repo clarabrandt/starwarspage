acb9518d1aca137bb3a82007fee7db44f7b45924
clarabrandt <mariaclarabrandt@gmail.com>
2019-07-05 10:27:38 +0200

    toggle button for people ASC or DESC height order

--- src/components/Content.js
+++ src/components/Content.js
@@ -2,17 +2,35 @@ import React, { Component } from 'react'
 import "./Content.css"
 
 export default class Content extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOrderAsc: true
+    };
+
+    this.toggleButton = this.toggleButton.bind(this);
+  }
+
+  toggleButton(e) {
+    console.log(this.state.isOrderAsc)
+    e.preventDefault();
+    this.setState(state => ({
+      isOrderAsc: !state.isOrderAsc
+    }));
+  }
   render() {
     return (
       <div className='content'>
         <div className='content-headline'>Search for the characters of you favorite Star Wars movie! </div>
         <div className='content-headline'>Search by the title or episode! </div>
-        <form onSubmit={this.handleSubmit}>
+        <form>
           <label>
             <div className='content-searchBox'>Input word(s) from Star Wars movie title:</div>
             <input type="text" name="name" />
           </label>
-          <button type="submit" value="" >Send</button>
+          <button onClick={this.toggleButton}>
+            {this.state.isOrderAsc ? 'ASC' : 'DESC'}
+          </button>
         </form>
       </div>
     )
