eaba0e60390eec5813e30334ad5cd909d0097048
clarabrandt <mariaclarabrandt@gmail.com>
2019-07-05 08:51:11 +0200

    form and button

--- src/components/Content.js
+++ src/components/Content.js
@@ -5,7 +5,15 @@ export default class Content extends Component {
   render() {
     return (
       <div className='content'>
-
+        <div className='content-headline'>Search for the characters of you favorite Star Wars movie! </div>
+        <div className='content-headline'>Search by the title or episode! </div>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            <div className='content-searchBox'>Input word(s) from Star Wars movie title:</div>
+            <input type="text" name="name" />
+          </label>
+          <button type="submit" value="" >Send</button>
+        </form>
       </div>
     )
   }
